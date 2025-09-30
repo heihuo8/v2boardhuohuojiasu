dfd0265de2a7f028d3dd2397fd47877c158d2549
heihuo8 <58242425+heihuo8@users.noreply.github.com>
2025-09-30 23:19:04 +0800

    Update 小火煎显示中文信息Shadowrocket.php
    小火煎显示中文信息

--- app/Protocols/Shadowrocket.php
+++ app/Protocols/Shadowrocket.php
@@ -25,9 +25,10 @@ class Shadowrocket
         //display remaining traffic and expire date
         $upload = round($user['u'] / (1024*1024*1024), 2);
         $download = round($user['d'] / (1024*1024*1024), 2);
+        $usedTraffic = $upload + $download;
         $totalTraffic = round($user['transfer_enable'] / (1024*1024*1024), 2);
         $expiredDate = date('Y-m-d', $user['expired_at']);
-        $uri .= "STATUS=🚀↑:{$upload}GB,↓:{$download}GB,TOT:{$totalTraffic}GB💡Expires:{$expiredDate}\r\n";
+        $uri .= "STATUS=🚀已用流量:{$usedTraffic}GB,总流量:{$totalTraffic}GB💡到期时间:{$expiredDate}\r\n";
 
         foreach ($this->servers as $server) {
             if ($server['type'] === 'vmess'){
